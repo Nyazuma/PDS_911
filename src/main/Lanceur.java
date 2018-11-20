25692b8ffa63924f9b2817bc39e239fefe445abf
PiJacquet <44034432+PiJacquet@users.noreply.github.com>
2018-11-20 11:51:40 +0100

    Fin des tests : suppression de l'architecture de projet inutile

--- src/main/Lanceur.java
+++ src/main/Lanceur.java
@@ -1,11 +1,10 @@
-package Defaut;
+package main;
 
 public class Lanceur {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
-		
 	}
 
 }
