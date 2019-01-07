8fc7a9a7124009b8acc76d72114ede1be076e176
Nyazuma <faizaan_94@hotmail.fr>
2019-01-07 14:14:59 +0100

    tentative de test

--- src/testObject/DataSourceTest.java
+++ src/testObject/DataSourceTest.java
@@ -2,13 +2,16 @@ package testObject;
 
 import static org.junit.Assert.*;
 
+import java.util.Properties;
+
 import org.junit.Test;
 
 public class DataSourceTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetInstanceConfig() {
+		System.out.println("getInstanceConfig");
+		Properties expResult = new Properties(configuration);
 	}
 
 }
