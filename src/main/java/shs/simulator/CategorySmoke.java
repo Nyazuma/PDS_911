d02e4b2b5fd2dde42a2de1c71b55b8865d0de8d0
PiJacquet <44034432+PiJacquet@users.noreply.github.com>
2019-04-16 10:12:40 +0200

    Création package simulator

---
+++ src/main/java/shs/simulator/CategorySmoke.java
@@ -0,0 +1,15 @@
+package shs.simulator;
+
+import java.util.List;
+
+public class CategorySmoke extends CategoryObject implements Runnable{
+
+	public CategorySmoke(String referencedName, List<String> listObjects, int askedNumberSensors) {
+		super(referencedName, listObjects, askedNumberSensors);
+	}
+
+	public void run() {
+		
+	}
+
+}
