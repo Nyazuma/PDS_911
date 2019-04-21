99acf5b01a8f8fe2c955fe34f9ef8548de23cc20
JAguesse <45197420+JAguesse@users.noreply.github.com>
2019-04-21 12:27:39 +0200

    Mise en place de la table des objets- en travaux

--- src/main/java/shs/gui/window/Map.java
+++ src/main/java/shs/gui/window/Map.java
@@ -1,5 +1,6 @@
 package shs.gui.window;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -20,6 +21,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 import shs.gui.GuiController;
@@ -50,9 +52,19 @@ public class Map extends JPanel implements ActionListener{
 	private GuiController controller; 
 	private String[] tabImage; 
 
-	private List<List<String>> listObject; 
+	/**
+	 * Gestion Table
+	 */
+	private JTable objectTable; 
+	private List<List<String>> listObject = new ArrayList<List<String>>(); 
+	private Object[][] data;
+	private JScrollPane scrollPane;
+	
+	/**
+	 * Gestion Emplacements
+	 */
 	private List<List<String>> listEmplacement;
-	private List<JButton> listJButtons; 
+	private List<JButton> listJButtonsEtage1; 
 	
 
 	public Map(GuiController controller) {
@@ -73,10 +85,14 @@ public class Map extends JPanel implements ActionListener{
 		btnRetour.addActionListener(this);
 		this.add(btnRetour);
 
-		table = new JTable();
-		table.setBounds(12, 99, 438, 527);
-		table.setFont(new Font("Cambria Math", Font.BOLD, 16));
-		this.add(table);
+//		table = new JTable();
+//		table.setBounds(12, 99, 438, 527);
+//		table.setFont(new Font("Cambria Math", Font.BOLD, 16));
+//		this.add(table);
+		
+		listObject = controller.readObjects();
+		gestionListObject();
+		this.add(scrollPane, BorderLayout.CENTER);
 
 		comboStage = new JComboBox<String>();
 		comboStage.setModel(new DefaultComboBoxModel(new String[] {"Etage 1", "Etage 2"}));
@@ -86,13 +102,13 @@ public class Map extends JPanel implements ActionListener{
 		this.add(comboStage);
 		
 		listEmplacement = controller.EmplacementFull(); 
-		listJButtons = new ArrayList<JButton>();
+		listJButtonsEtage1 = new ArrayList<JButton>();
 		
 		for(int ligne = 0; ligne< listEmplacement.size(); ligne++) {
 			if(listEmplacement.get(ligne).get(2).toString().equals("1")  && comboStage.getSelectedItem().toString().equals("Etage 1") ) {
 			JButton newButton = new JButton(); 
 			newButton.setBounds(Integer.parseInt(listEmplacement.get(ligne).get(3)), Integer.parseInt(listEmplacement.get(ligne).get(4)), Integer.parseInt(listEmplacement.get(ligne).get(5)), Integer.parseInt(listEmplacement.get(ligne).get(6)));
-			listJButtons.add(newButton); 
+			listJButtonsEtage1.add(newButton); 
 			this.add(newButton);
 			}
 		}
@@ -112,6 +128,52 @@ public class Map extends JPanel implements ActionListener{
 		picLabel = new JLabel(imageIcon); 
 		picLabel.setBounds(491, 99, 1345, 824);
 		this.add(picLabel);
+
+	}
+	
+	public void gestionListObject() {
+		String[] header = {"ID_capteur", "Type du capteur",  "Etat capteur", "Mac"}; 
+		Integer x = listObject.size(); 
+		Integer y;
+		if(x>0) { 
+			System.out.println("Je suis la ");
+			// If the result is not empty, we could fill our table
+			y = listObject.get(0).size();
+			data = new Object[x][y]; 
+			Integer i = 0;
+			Integer j = 0;
+			for(List<String> line : listObject) {
+				for(String column : line) {
+					if(j!=2)
+						data[i][j]=column;
+					else {
+						// Status
+						if(column.equals("1")) 
+							data[i][2] = "Enable"; 
+						else 
+							data[i][2] = "Disable"; 
+					}
+					j++;
+				}
+				j=0;
+				i++;
+			}
+
+			objectTable = new JTable(data, header);
+			// We cancel the user selection of a table case.
+			objectTable.setDefaultEditor(Object.class, null);
+			//Hide ID column
+			objectTable.getColumnModel().getColumn(0).setMinWidth(0);
+			objectTable.getColumnModel().getColumn(0).setMaxWidth(0);
+		}
+		else {
+			// if the result is empty, the table will be empty
+			objectTable = new JTable();
+		}
+
+
+		scrollPane = new JScrollPane(objectTable);
+		scrollPane.setBounds(12, 99, 438, 527);
 
 	}
 
