0abd592f92400394acf7e50d78883353a63f2c24
PiJacquet <44034432+PiJacquet@users.noreply.github.com>
2019-04-22 16:57:06 +0200

    Ajout de parametres au cache, thread de nettoyage

--- src/main/java/shs/server/MemoryCache.java
+++ src/main/java/shs/server/MemoryCache.java
@@ -11,19 +11,27 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MemoryCache implements Runnable {
 
 
-	private static Map<Integer, List<Timestamp>> cache = new ConcurrentHashMap<Integer, List<Timestamp>>();
+	private static Map<Integer, List<List<Object>>> cache = new ConcurrentHashMap<Integer, List<List<Object>>>();
 	// TODO : could be useful to put these numbers in the config file
 	final protected static Integer occurenciesNecessary = 3;
-	final protected static Integer delayTime = 5*60*1000; // max delay which defined that two events are linked
-
+	final protected static Integer delayTime = 5*60*1000; // max delay which defines that two events are linked
+	final protected static Integer frequenceClearTime = 60*60*1000;
+	
 	public static boolean addCacheData(Integer id) {
+		return addCacheData(id, null);
+	}
+	
+	public static boolean addCacheData(Integer id, Float value) {
 		if(cache.containsKey(id)){
-			cache.get(id).add(new Timestamp(System.currentTimeMillis()));
+			ArrayList<Object> line = new ArrayList<Object>();
+			line.add(new Timestamp(System.currentTimeMillis()));
+			line.add(value);
+			cache.get(id).add(line);
 			// We will count all the events which occurs
 			int occurencies = 0;
 			Timestamp limitTime = new Timestamp(System.currentTimeMillis()-delayTime);
-			for(Timestamp time : cache.get(id)) {
-				if(time.after(limitTime)) {
+			for(List<Object> cursor : cache.get(id)) {
+				if(((Timestamp) cursor.get(0)).after(limitTime)) {
 					occurencies++;
 				}
 			}
@@ -33,22 +41,46 @@ public class MemoryCache implements Runnable {
 				return false;
 		}
 		else {
-			List<Timestamp> list = new ArrayList<Timestamp>();
-			list.add(new Timestamp(System.currentTimeMillis()));
-			cache.put(id, list);
+			List<Object> lineSensors = new ArrayList<Object>();
+			List<List<Object>> infoSensors = new ArrayList<List<Object>>();
+			lineSensors.add(new Timestamp(System.currentTimeMillis()));
+			lineSensors.add(value);
+			infoSensors.add(lineSensors);
+			cache.put(id, infoSensors);
 			return false;
 		}
 	}
+	
+	public static Float getAverageValue(Integer id) {
+		Float average = 0.0f;
+		int occurencies = 0;
+		if(cache.containsKey(id)){
+			List<List<Object>> reportsSensor = cache.get(id);
+			Timestamp limitTime = new Timestamp(System.currentTimeMillis()-delayTime);
+			for(List<Object> cursor : reportsSensor) {
+				if(((Timestamp) cursor.get(0)).after(limitTime) && cursor.get(1)!=null) {
+					average+= (Float) cursor.get(1);
+					occurencies++;
+				}
+			}
+		}
+		if(occurencies==0)
+			return null;
+		else {
+			average = average / occurencies;
+			return average;
+		}			
+	}
 
 	public static void clearOldData() {
-		// We will remove all the timestamp which are older than 24h
-		Timestamp limitDate = new Timestamp(System.currentTimeMillis()-100);
+		// We will remove all the timestamp which are older than clearTime
+		Timestamp limitDate = new Timestamp(System.currentTimeMillis()-delayTime);
 		Iterator<Integer> iteratorID = cache.keySet().iterator();
 		while(iteratorID.hasNext()){
 			Integer id = iteratorID.next();
-			ListIterator<Timestamp> iteratorTimestamp = cache.get(id).listIterator();
+			ListIterator<List<Object>> iteratorTimestamp = cache.get(id).listIterator();
 			while(iteratorTimestamp.hasNext()) {
-				Timestamp time = iteratorTimestamp.next();
+				Timestamp time = (Timestamp) iteratorTimestamp.next().get(0);
 				if(time.before(limitDate)) {
 					iteratorTimestamp.remove();
 				}
@@ -61,12 +93,12 @@ public class MemoryCache implements Runnable {
 	public void run() {
 		while(true) {
 			try {
-				//We clear the old data each hours;
-				Thread.sleep(60*60*1000);
+				Thread.sleep(frequenceClearTime);
 				clearOldData();
 			}catch(InterruptedException e) {
 				break;
 			}
 		}
 	}
+	
 }
