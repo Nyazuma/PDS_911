276caa83e17ab4e39602fae43415ac0cf32314eb
PiJacquet <44034432+PiJacquet@users.noreply.github.com>
2019-05-14 12:19:06 +0200

    Correction d'une alerte

--- src/main/java/shs/simulator/CategoryHygro.java
+++ src/main/java/shs/simulator/CategoryHygro.java
@@ -10,21 +10,20 @@ public class CategoryHygro extends CategoryObject{
 	public CategoryHygro(String referencedName, List<String> listObjects) {
 		super(referencedName, listObjects);
 	}
-	
+
 	public CategoryHygro() {
 		super();
 	}
 
 	public void launchAlert(Integer id) {
-		for(int i=0; i<3; i++) {
-			Integer hygroValue = DataConfigSimu.getMIN_HYGRO_VALUE() + 
-					(int) (Math.random()*(DataConfigSimu.getMAX_HYGRO_VALUE()-DataConfigSimu.getMIN_HYGRO_VALUE()));
-			MsgReportHygro update = new MsgReportHygro(id, hygroValue);
-			Connector.contactServer(Tool.messageToJSON(update));
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {}
-		}
+		Integer hygroValue = DataConfigSimu.getMIN_HYGRO_VALUE() + 
+				(int) (Math.random()*(DataConfigSimu.getMAX_HYGRO_VALUE()-DataConfigSimu.getMIN_HYGRO_VALUE()));
+		MsgReportHygro update = new MsgReportHygro(id, hygroValue);
+		Connector.contactServer(Tool.messageToJSON(update));
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {}
 	}
+
 
 }
